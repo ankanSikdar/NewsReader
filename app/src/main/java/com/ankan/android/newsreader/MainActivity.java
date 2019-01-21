d3f712cd574a02e7552bd88b65eaf02a95bb629a
Ankan Sikdar <ankansikdar@gmail.com>
2019-01-21 21:48:54 +0530

    Getting the ids correctly from the API.

--- app/src/main/java/com/ankan/android/newsreader/MainActivity.java
+++ app/src/main/java/com/ankan/android/newsreader/MainActivity.java
@@ -1,16 +1,27 @@
 package com.ankan.android.newsreader;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    ArrayList<String> arrayList;
+    static ArrayList<String> idArrayList;
+    static ArrayList<String> titleArrayList;
+    static ArrayList<String> urlArrayList;
     ArrayAdapter<String> arrayAdapter;
 
     @Override
@@ -19,11 +30,100 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
-        arrayList = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, arrayList);
+        idArrayList = new ArrayList<>();
+        titleArrayList = new ArrayList<>();
+        urlArrayList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, idArrayList);
 
-        arrayList.add("1. Hello");
-        arrayList.add("2. You");
-        listView.setAdapter(arrayAdapter);
+        IdDownloadTask downloadTask = new IdDownloadTask();
+        downloadTask.execute("https://hacker-news.firebaseio.com/v0/topstories.json");
+        Log.i("id size", Integer.toString(idArrayList.size()));
+
+
+    }
+
+    public class IdDownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+            String result = "";
+
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while (data != -1) {
+                    char currentChar = (char) data;
+                    result += currentChar;
+                    data = reader.read();
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Log.i("Result", s);
+                JSONArray jsonArray = new JSONArray(s);
+                for(int i = 0; i < jsonArray.length(); i++) {
+                    idArrayList.add(jsonArray.get(i).toString());
+                    arrayAdapter.notifyDataSetChanged();
+                }
+                listView.setAdapter(arrayAdapter);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class NewsDownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+            String result = "";
+
+            try {
+                Log.i("Title", "Executing");
+                url = new URL("https://hacker-news.firebaseio.com/v0/item/" + urls[0] + ".json");
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = httpURLConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+                while (data != -1) {
+                    char currentChar = (char) data;
+                    result += currentChar;
+                    data = reader.read();
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String title = jsonObject.getString("title");
+                titleArrayList.add(title);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
